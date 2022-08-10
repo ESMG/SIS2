b20a04041bea869ec5d507d362e9144f3a1bf8c4
Kate Hedstrom <kshedstrom@alaska.edu>
2022-08-10 15:03:38 -0800

    Try zeroing aice when mass goes negative.
    - didn't help

--- src/ice_ridge.F90
+++ src/ice_ridge.F90
@@ -440,6 +440,7 @@ subroutine ice_ridging(IST, G, IG, mca_ice, mca_snow, mca_pond, TrReg, CS, US, d
             write(mesg,'("Negative ice volume after ridging: ", i6, i6, 2x, 1pe12.4, 1pe12.4)')  &
                           i+G%idg_offset, j+G%jdg_offset, aicen(k), vicen(k)
             call SIS_error(WARNING, mesg, all_print=.true.)
+            aicen(k) = 0.0
             vicen(k) = 0.0
             vsnon(k) = 0.0
           endif
